df0f22d5469745e144a50e3085f13a85ebe80a3d
Yusong Hsu <sky172839465@gmail.com>
2025-11-03 00:10:01 +0800

    Sky172839465 patch 1 (#15)
    * Update cf-r2-sync.yml
    
    * Create getImageSize.js
    
    * Update cf-r2-sync.yml
    
    * Update cf-r2-sync.yml

---
+++ scripts/getImageSize.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import sizeOf from 'image-size';
+
+const file = process.argv[2];
+
+try {
+  const dimensions = sizeOf(file);
+  console.log(JSON.stringify({ name: file.split('/').pop(), width: dimensions.width, height: dimensions.height }));
+} catch (err) {
+  console.log(JSON.stringify({ name: JSON.stringify(file.split('/').pop())) }); // fallback for non-image
+}
