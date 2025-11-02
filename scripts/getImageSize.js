b445ccff7b4c0ea1dd4cbace0501ceff60b0b88c
Yusong Hsu <sky172839465@gmail.com>
2025-11-03 00:12:52 +0800

    Update getImageSize.js

--- scripts/getImageSize.js
+++ scripts/getImageSize.js
@@ -7,5 +7,9 @@ try {
   const dimensions = sizeOf(file);
   console.log(JSON.stringify({ name: file.split('/').pop(), width: dimensions.width, height: dimensions.height }));
 } catch (err) {
-  console.log(JSON.stringify({ name: JSON.stringify(file.split('/').pop())) }); // fallback for non-image
+  console.log(
+    JSON.stringify({
+      name: JSON.stringify(file.split('/').pop())
+    })
+  ); // fallback for non-image
 }
